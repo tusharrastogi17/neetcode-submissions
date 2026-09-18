dc0e690e53530c506565134077153a550c20d2a8
tusharrastogi17 <68027060+tusharrastogi17@users.noreply.github.com>
2026-09-18 12:32:39 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(!(s.length()==t.length())){
+            return false;
+        }
+        else{
+            int arr[] = new int[256];
+
+            for(int i=0; i<s.length(); i++){
+                arr[s.charAt(i)]++;
+                arr[t.charAt(i)]--;
+            }
+
+            for(int i=0; i<250; i++){
+                if(arr[i]>0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+}
